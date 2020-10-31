d5a290d7e64f8c31384d480cca720007607df969
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 14:36:34 -0700

    Change vsf typo to vcf

--- code/00_setup_directories.sh
+++ code/00_setup_directories.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p /data/sars_vsf_analysis/01_raw_fastq
-mkdir -p /data/sars_vsf_analysis/02_genome_reference
-mkdir -p /data/sars_vsf_analysis/03_fastqc_output
-mkdir -p /data/sars_vsf_analysis/04_trimmed_fastq
-mkdir -p /data/sars_vsf_analysis/05_mapped_sam
-mkdir -p /data/sars_vsf_analysis/06_mapped_bam
-mkdir -p /data/sars_vsf_analysis/07_mapped_sorted_bam
-mkdir -p /data/sars_vsf_analysis/08_bcf_variants
-mkdir -p /data/sars_vsf_analysis/09_vcf_called
+mkdir -p /data/sars_vcf_analysis/01_raw_fastq
+mkdir -p /data/sars_vcf_analysis/02_genome_reference
+mkdir -p /data/sars_vcf_analysis/03_fastqc_output
+mkdir -p /data/sars_vcf_analysis/04_trimmed_fastq
+mkdir -p /data/sars_vcf_analysis/05_mapped_sam
+mkdir -p /data/sars_vcf_analysis/06_mapped_bam
+mkdir -p /data/sars_vcf_analysis/07_mapped_sorted_bam
+mkdir -p /data/sars_vcf_analysis/08_bcf_variants
+mkdir -p /data/sars_vcf_analysis/09_vcf_called
 
 # note this is in repo not in root /data
 mkdir -p data/00_sra_runtable
