27be572fb6d365e28324efa6ab86141a78b8681b
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 16:18:53 -0700

    Draft flagstats script and renumber output dirs

--- code/00_setup_directories.sh
+++ code/00_setup_directories.sh
@@ -9,9 +9,10 @@ mkdir -p /data/sars_vcf_analysis/04_trimmed_fastq
 mkdir -p /data/sars_vcf_analysis/05_mapped_sam
 mkdir -p /data/sars_vcf_analysis/06_mapped_bam
 mkdir -p /data/sars_vcf_analysis/07_mapped_sorted_bam
-mkdir -p /data/sars_vcf_analysis/08_bcf_variants
-mkdir -p /data/sars_vcf_analysis/09_vcf_called
+mkdir -p /data/sars_vcf_analysis/08_flagstats
+mkdir -p /data/sars_vcf_analysis/09_bcf_variants
+mkdir -p /data/sars_vcf_analysis/10_vcf_called
 
 # note this is in repo not in root /data
 mkdir -p data/00_sra_runtable
-mkdir -p data/10_vcf_output_for_R
+mkdir -p data/11_vcf_output_for_R
--- code/09_flagstat.sh
+++ code/09_flagstat.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-samtools flagstat output.sorted.bam
+OUTPUT_DIR="/data/sars_vcf_analysis/08_flagstats/"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will run flagstats on the sorted bam files it is given."
+    echo "Please supply a set of sorted bam files as arguments."
+    exit 1
+fi
+
+# run trimmomatic to throw out bad sequences, trim when quality gets low
+for sorted_bam_file in "$@"
+do
+    echo "Running flagstats on ${sorted_bam_file}..."
+    samtools flagstat "$sorted_bam_file" > "${OUTPUT_DIR}$(basename $sorted_bam_file).stats.txt"
+done
+
