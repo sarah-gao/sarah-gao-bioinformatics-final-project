5e35881dae5fb73aa9646e65a05369f08c9e1d1f
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 16:19:14 -0700

    Add code for sorting bam files

--- code/08_sort_bam.sh
+++ code/08_sort_bam.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-samtools sort -o output.sorted.bam output.bam
+OUTPUT_DIR="/data/sars_vcf_analysis/07_mapped_sorted_bam/"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will sort the bam files it is given."
+    echo "Please supply a set of bam files as arguments."
+    exit 1
+fi
+
+# run trimmomatic to throw out bad sequences, trim when quality gets low
+for bam_file in "$@"
+do
+    echo "Sorting ${bam_file}..."
+    samtools sort -o "${OUTPUT_DIR}$(basename -s .bam $bam_file).sorted.bam" "$bam_file"
+done
+
