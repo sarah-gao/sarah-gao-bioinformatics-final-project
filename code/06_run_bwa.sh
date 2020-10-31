2d6789c43e1dc6f6d2c47b1dc3a9caaea04ec39e
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 15:42:50 -0700

    Add code for running bwa

--- code/06_run_bwa.sh
+++ code/06_run_bwa.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-bwa mem refgenome.fasta SRR12433063_1.fastq SRR12433063_2.fastq > output.sam
+OUTPUT_DIR="/data/sars_vcf_analysis/05_mapped_sam/"
+REF_GENOME="/data/sars_vcf_analysis/02_genome_reference/sars_refgenome.fasta"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will run bwa on the files it is given."
+    echo "Please supply a set of fastq files as arguments."
+    exit 1
+fi
+
+# run trimmomatic to throw out bad sequences, trim when quality gets low
+for seq_file in "$@"
+do
+    bwa mem "$REF_GENOME" "$seq_file" > "${OUTPUT_DIR}$(basename $seq_file).bwa-output.sam"
+done
+
