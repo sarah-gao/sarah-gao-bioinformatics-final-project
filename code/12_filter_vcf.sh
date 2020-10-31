bb6a7ac0549e0b02662657a52ee7374b6dfd3c28
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 16:39:57 -0700

    Add code to filter vcfs

--- code/12_filter_vcf.sh
+++ code/12_filter_vcf.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+OUTPUT_DIR="data/11_vcf_output_for_R/"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will filter short variants on vcf files it is given."
+    echo "Please supply a set of vcf files as arguments."
+    exit 1
+fi
+
+for vcf_file in "$@"
+do
+    echo "Filtering ${vcf_file}..."
+    vcfutils.pl varFilter "$vcf_file" > "${OUTPUT_DIR}$(basename -s .variants.vcf "$vcf_file").final-variants.vcf"
+done
+
