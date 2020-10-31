43824e2a3947deab257c02488b7ac39fecfb0114
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 16:39:32 -0700

    Add code for mpileup

--- code/10_bcftools_mpileup.sh
+++ code/10_bcftools_mpileup.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-bcftools mpileup -O b -o output_raw.bcf -f refgenome.fasta output.sorted.bam
+OUTPUT_DIR="/data/sars_vcf_analysis/09_bcf_variants/"
+REF_GENOME="/data/sars_vcf_analysis/02_genome_reference/sars_refgenome.fasta"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will call variants on the sorted bam files it is given."
+    echo "Please supply a set of sorted bam files as arguments."
+    exit 1
+fi
+
+# run trimmomatic to throw out bad sequences, trim when quality gets low
+for sorted_bam_file in "$@"
+do
+    bcftools mpileup -O b -o "${OUTPUT_DIR}$(basename "$sorted_bam_file").raw.bcf" -f "$REF_GENOME" "$sorted_bam_file"
+done
+
