e76c43f9a8fcb38bb80a3a13280ecbbab7b725e3
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 15:34:19 -0700

    Add code for bwa index

--- code/05_bwa_index.sh
+++ code/05_bwa_index.sh
@@ -2,4 +2,7 @@
 
 set -euo pipefail
 
-bwa index refgenome.fasta > refgenome.idx
+REF_DIR="/data/sars_vcf_analysis/02_genome_reference/"
+
+# index the reference genome for bwa
+bwa index "${REF_DIR}sars_refgenome.fasta" > "${REF_DIR}sars_refgenome.fasta.idx"
