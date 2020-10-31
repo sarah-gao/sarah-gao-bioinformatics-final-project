93e2aba7d710b4ae168de4ed5b916e4645a8c4c0
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 15:00:19 -0700

    Add code for fastqc

--- code/03_fastqc.sh
+++ code/03_fastqc.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+OUTPUT_DIR="/data/sars_vcf_analysis/"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will run fastqc on the files it is given."
+    echo "Please supply a set of fastq files as arguments."
+    exit 1
+fi
+
+# run fastqc on all fastq files given as command-line input
+fastqc -o /data/sars_vcf_analysis/03_fastqc_output -f fastq "$@"
