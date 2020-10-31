a418d2de0207cf028aa1cf47abbda91740448929
Naupaka Zimmerman <naupaka@gmail.com>
2020-10-31 15:10:59 -0700

    Add code for Trimmomatic script

--- code/04_trim.sh
+++ code/04_trim.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+OUTPUT_DIR="/data/sars_vcf_analysis/04_trimmed_fastq/"
+TRIM_PARAMS="LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:200"
+
+if [ $# -eq 0 ]
+then
+    echo "This script will run trimmomatic on the files it is given."
+    echo "Please supply a set of fastq files as arguments."
+    exit 1
+fi
+
+# run trimmomatic to throw out bad sequences, trim when quality gets low
+for seq_file in "$@"
+do
+    TrimmomaticSE -threads 4 -phred33 "$seq_file" "${OUTPUT_DIR}$(basename -s .fastq "$seq_file").trim.fastq" "$TRIM_PARAMS" 
+done
