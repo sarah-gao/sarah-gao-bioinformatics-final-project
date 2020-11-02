500e714e0818d27a66f6cbfbbbd32b42c350b037
Naupaka Zimmerman <naupaka@gmail.com>
2020-11-02 01:32:29 +0000

    Also download reference genome annotation

--- code/02_download_reference.sh
+++ code/02_download_reference.sh
@@ -2,12 +2,25 @@
 
 set -euo pipefail
 
-# this script downloads the reference genome fasta file for SARS-CoV-2
+# this script downloads the reference genome fasta file and annotation gff for SARS-CoV-2
 
-BASE_URL="https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi"
-URL_PART2="?id=NC_045512.2&db=nuccore&report=fasta&retmode=text&"
-URL_PART3="withmarkup=on&tool=portal&log$=seqview&maxdownloadsize=100000000"
+GENOME_BASE_URL="https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi"
+GENOME_URL_PART2="?id=NC_045512.2&db=nuccore&report=fasta&retmode=text&"
+GENOME_URL_PART3="withmarkup=on&tool=portal&log$=seqview&maxdownloadsize=100000000"
 
-OUTPUT_FILE="/data/sars_vcf_analysis/02_genome_reference/sars_refgenome.fasta"
+ANNOTATION_BASE_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/858/895/"
+ANNOTATION_URL_PART2="GCF_009858895.2_ASM985889v3/GCF_009858895.2_ASM985889v3_genomic.gff.gz"
 
-curl "${BASE_URL}${URL_PART2}${URL_PART3}" > "$OUTPUT_FILE"
+GENOME_OUTPUT_FILE="/data/sars_vcf_analysis/02_genome_reference/sars_refgenome.fasta"
+ANNOTATION_OUTPUT_FILE="/data/sars_vcf_analysis/02_genome_reference/sars_refgenome_annotation.gff.gz"
+
+# download genome
+echo "Downloading SARS-CoV-2 reference genome..."
+curl "${GENOME_BASE_URL}${GENOME_URL_PART2}${GENOME_URL_PART3}" > "$GENOME_OUTPUT_FILE"
+
+# download genome annotation
+echo "Downloading SARS-CoV-2 reference genome annotation..."
+curl "${ANNOTATION_BASE_URL}${ANNOTATION_URL_PART2}" --output "${ANNOTATION_OUTPUT_FILE}"
+
+# extract genome annotation
+gunzip -v "${ANNOTATION_OUTPUT_FILE}"
